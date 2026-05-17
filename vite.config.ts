8779f51c485572ce8acd20e357aaed6c3760c39d
Stijn Janmaat <stijnjanmaat@gmail.com>
2026-05-17 11:06:25 +0200

    use nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import react from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tanstackStart(), tailwindcss(), react()],
+  plugins: [tanstackStart(), nitro(), tailwindcss(), react()],
 });
